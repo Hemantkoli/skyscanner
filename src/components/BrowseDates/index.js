598d41dc26f8a4aa2ba7b783d397c0f882babcd4
Hemantkoli14321 <hemantkoli14321@gmail.com>
2021-03-25 17:17:40 +0530

    Update index.js

--- src/components/BrowseDates/index.js
+++ src/components/BrowseDates/index.js
@@ -1,9 +1,11 @@
 import { Container } from "react-bootstrap";
 const BrowseDates = (props)=>{
-    if(!props.cheapestDates.length)
-    {return null;}
+    //if(!props.cheapestDates.length)
+    //{return null;}
+
     const constructTable = (CheapDateList)=>{
         return(
+            
             <div id="main_div">
             {CheapDateList.map(index => 
             <div key={index.Carriers.Name} id="second_main_div">
@@ -11,6 +13,10 @@ const BrowseDates = (props)=>{
                 Price: {index.MinPrice}<br/>
                 DepartureDate: {index.OutboundLeg.DepartureDate}<br/>
                 Carriers: {index.Carriers.Name}
+                {props.DateFlag
+                ?<><br/>ReturnDate: {index.InboundLeg.DepartureDate}</>
+                :<></>
+                }         
             </div>
             )}
             </div>
